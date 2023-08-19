ee88fedd475c0d348785158b1be46573d0bbd957
xxcosita3czxx <kokosove18@gmail.com>
2023-08-19 10:42:15 +0200

    update

--- utils/grabber.py
+++ utils/grabber.py
@@ -1,4 +1,4 @@
 import os
 import grabber_utils.build
 def main():
-    pass
+    print("GRABBER BUILDER")
---
+++ utils/grabber_utils/CODE.py
@@ -0,0 +1,6 @@
+import discord_webhooks
+
+def main():
+    pass
+if __name__=="__main__":
+    main()
