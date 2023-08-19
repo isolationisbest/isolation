aa76d232b8fd475b068a9ceb937fad8b65f26f5a
xxcosita3czxx <kokosove18@gmail.com>
2023-08-19 14:36:58 +0200

    uploud

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ try:
   from time import sleep
   from pystyle import Write, Colors, Colorate, Box
   from colorama import Fore, Back, Style
-
+  import socket
+  hostname=socket.gethostname()
   colorama.init()
   import time
   import random
-
+  import sys
   def clear_console():
       if os.name == 'nt':  
           os.system('cls')
@@ -39,105 +40,119 @@ try:
   if __name__ == "__main__":
       loading_screen()
       print("\nLoading completed! ")
+  while True:
+    clear_console()
+
+    Write.Print(f'''
+      ██╗███████╗ ██████╗ ██╗      █████╗ ████████╗██╗ ██████╗ ███╗   ██╗
+      ██║██╔════╝██╔═══██╗██║     ██╔══██╗╚══██╔══╝██║██╔═══██╗████╗  ██║
+      ██║███████╗██║   ██║██║     ███████║   ██║   ██║██║   ██║██╔██╗ ██║
+      ██║╚════██║██║   ██║██║     ██╔══██║   ██║   ██║██║   ██║██║╚██╗██║
+      ██║███████║╚██████╔╝███████╗██║  ██║   ██║   ██║╚██████╔╝██║ ╚████║
+      ╚═╝╚══════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
+            
+        ╔══════════════════════════════════════════════════════════════╗
+        ║ [1] Nitro Gen + Checker              [10]                    ║  
+        ║ [2] Webhook Spammer                  [11]                    ║
+        ║ [3] CC Generator                     [12]                    ║
+        ║ [4] Netflix Generator                [13]                    ║
+        ║ [5] Xbox GT Generator                [14]                    ║
+        ║ [6] Hacking                                                  ║
+        ║ [7] Doxxing                                                  ║
+        ║ [8] Token Checker                            [00] CREDITS    ║
+        ║ [9] Token Grabber                            [99] EXIT       ║
+        ╚══════════════════════════════════════════════════════════════╝
+    ''', Colors.blue_to_cyan, interval=0.005)
+
+    print(Fore.LIGHTBLUE_EX)
+    choice = int(input("Choice:  "))
+
+    if choice == 1:
+      import os
+      import ctypes
+      import requests
+      import random
+      import string
+      import time
+      print("")
       
-  clear_console()
+      clear_console()
 
-  Write.Print(f'''
-    ██╗███████╗ ██████╗ ██╗      █████╗ ████████╗██╗ ██████╗ ███╗   ██╗
-    ██║██╔════╝██╔═══██╗██║     ██╔══██╗╚══██╔══╝██║██╔═══██╗████╗  ██║
-    ██║███████╗██║   ██║██║     ███████║   ██║   ██║██║   ██║██╔██╗ ██║
-    ██║╚════██║██║   ██║██║     ██╔══██║   ██║   ██║██║   ██║██║╚██╗██║
-    ██║███████║╚██████╔╝███████╗██║  ██║   ██║   ██║╚██████╔╝██║ ╚████║
-    ╚═╝╚══════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
-          
-      ╔══════════════════════════════════════════════════════════════╗
-      ║ [1] Nitro Gen + Checker                    [10]        ║  
-      ║ [2] Webhook Spammer                 [11]             ║
-      ║ [3] CC Generator                [12]        ║
-      ║ [4] Netflix Generator                [13]           ║
-      ║ [5] Xbox GT Generator                       [14]         ║
-      ║ [6] Hacking                                                ║
-      ║ [7] Doxxing                [15] CREDITS               ║
-      ║ [8] Token Checker             [17] EXIT                 ║
-      ║ [9] Token Grabber                                            ║
-      ╚══════════════════════════════════════════════════════════════╝
-  ''', Colors.blue_to_cyan, interval=0.005)
+      num = int(input('How much codes to generate and check?: '))
 
-  print(Fore.LIGHTBLUE_EX)
-  choice = int(input("Choice:  "))
+      with open("Nitro Codes.txt", "w", encoding='utf-8') as file:
+        print("Your nitro codes are being generated!")
 
-  if choice == 1:
-    import os
-    import ctypes
-    import requests
-    import random
-    import string
-    import time
-    print("")
-    
-    clear_console()
+        start = time.time()
 
-    num = int(input('How much codes to generate and check?: '))
+        for i in range(num):
+          code = "".join(
+            random.choices(string.ascii_uppercase + string.digits +
+                          string.ascii_lowercase,
+                          k=19))
 
-    with open("Nitro Codes.txt", "w", encoding='utf-8') as file:
-      print("Your nitro codes are being generated!")
+          file.write(f"https://discord.gift/{code}\n")
 
-      start = time.time()
+      with open("Nitro Codes.txt") as file:
+        for line in file.readlines():
+          nitro = line.strip("\n")
 
-      for i in range(num):
-        code = "".join(
-          random.choices(string.ascii_uppercase + string.digits +
-                        string.ascii_lowercase,
-                        k=19))
+          url = "https://discordapp.com/api/v6/entitlements/gift-codes/" + nitro + "?with_application=false&with_subscription_plan=true"
 
-        file.write(f"https://discord.gift/{code}\n")
+          r = requests.get(url)
 
-    with open("Nitro Codes.txt") as file:
-      for line in file.readlines():
-        nitro = line.strip("\n")
+          if r.status_code == 200:
+            print( f" Valid | {nitro} ")
+            break
+          else:
+            print( f" Invalid | {nitro} ")
 
-        url = "https://discordapp.com/api/v6/entitlements/gift-codes/" + nitro + "?with_application=false&with_subscription_plan=true"
+      time.sleep(0.2)
 
-        r = requests.get(url)
+      print(
+        "Valid codes will be in Valid Codes.txt - if there none then keep generating :)"
+      )
+      time.sleep(3)
+    elif choice == 2:
+      import requests
+      from discord_webhooks import DiscordWebhooks
+      
+      clear_console()
 
-        if r.status_code == 200:
-          print( f" Valid | {nitro} ")
-          break
-        else:
-          print( f" Invalid | {nitro} ")
+      webhook_url = input('Webhook URL: ')
 
-    time.sleep(0.2)
-
-    print(
-      "Valid codes will be in Valid Codes.txt - if there none then keep generating :)"
-    )
-
-  elif choice == 2:
-    import requests
-    from discord_webhooks import DiscordWebhooks
-    
-    clear_console()
-
-    webhook_url = input('Webhook URL: ')
-
-    message = input('Enter Message: ')
-    ilosc = int(input('Enter Amount of Messages: '))
+      message = input('Enter Message: ')
+      ilosc = int(input('Enter Amount of Messages: '))
 
 
-    webhook = DiscordWebhooks(webhook_url)
+      webhook = DiscordWebhooks(webhook_url)
 
-    webhook.set_content(title='get spammed nigga ass cock',
-                      description=message,
-                      color=0xF58CBA)
+      webhook.set_content(title='get spammed nigga ass cock',
+                        description=message,
+                        color=0xF58CBA)
 
-    webhook.set_footer(text='made w/ isolation')
+      webhook.set_footer(text='made w/ isolation')
 
 
 
-    for i in range(ilosc):
-        webhook.send()
-  elif choice == 9:
-    print("yet working on")
-
+      for i in range(ilosc):
+          webhook.send()
+    elif choice == 9:
+      print("yet working on")
+    elif choice == 99:
+        Write.Print(f'''
+      ██╗███████╗ ██████╗ ██╗      █████╗ ████████╗██╗ ██████╗ ███╗   ██╗
+      ██║██╔════╝██╔═══██╗██║     ██╔══██╗╚══██╔══╝██║██╔═══██╗████╗  ██║
+      ██║███████╗██║   ██║██║     ███████║   ██║   ██║██║   ██║██╔██╗ ██║
+      ██║╚════██║██║   ██║██║     ██╔══██║   ██║   ██║██║   ██║██║╚██╗██║
+      ██║███████║╚██████╔╝███████╗██║  ██║   ██║   ██║╚██████╔╝██║ ╚████║
+      ╚═╝╚══════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
+            
+        ╔══════════════╗
+        ║ Bye {hostname}
+        ╚══════════════╝
+    ''', Colors.blue_to_cyan, interval=0.0005)
+    sys.exit()
 except KeyboardInterrupt:
+  print("")
   print("bhai :3")
