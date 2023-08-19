a1b97d0987be4fed65e3004afba07be1e57bd914
xxcosita3czxx <kokosove18@gmail.com>
2023-08-19 21:49:35 +0200

    some modifiin

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-echo WARNING >> this doesnt need to work, linux cross work isnt guarranteed!!
+echo WARNING: this doesnt need to work, linux cross work isnt guarranteed!!
 echo continuing...
 
 sudo apt install python3 pip3
